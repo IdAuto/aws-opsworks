18c28fc330653c617f4879c008fc93d0b2255b4c
jseiser <justinseiser@gmail.com>
2014-12-19 15:08:36 -0500

    Added Capistrano

---
+++ j-wordpress/recipes/capistrano.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: j-wordpress
+# Recipe:: composer
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+gem_package "capistrano" do
+  action :install
+end
