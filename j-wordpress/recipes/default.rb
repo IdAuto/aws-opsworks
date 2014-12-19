526f569d9973b405012d7f8d0990f623f898ac4c
jseiser <justinseiser@gmail.com>
2014-12-19 11:36:16 -0500

    First Cookbook

---
+++ j-wordpress/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: j-wordpress
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "httpd" do
+  action :action # see actions section below
+end
+
+service "httpd" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+cookbook_file '/var/www/html/index.html' do
+  source 'index.html'
+  mode '0644'
+end
