295efdde642ad15a543aae40516d7b8d1a06cc1e
Troy Moreland <tmoreland@idauto.net>
2014-12-20 22:29:44 -0600

    Completed first attempt at complete recipe -- BUG
    Tomcat is not starting so obviously more work to do.

--- idautoapps/cookbooks/idautoapps/recipes/default.rb
+++ idautoapps/cookbooks/idautoapps/recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+###@@@  STATUS: Recipe believed to contain everything; however, Tomcat is not starting!
+
 ###########################################################################
 # Perform OS Updates                                                      #
 ###########################################################################
@@ -41,49 +43,6 @@ end
 # Install LDAP client package
 package "openldap-clients" do
 	action :install
-end
-
-
-###########################################################################
-# Set timezone                                                            #
-###########################################################################
-timezone = "America/Chicago"
-
-link "/etc/localtime" do
-	to "/usr/share/zoneinfo/#{timezone}"
-	not_if "readlink /etc/localtime | grep -q '#{timezone}$'"
-end
-
-###########################################################################
-# Set hostname                                                            #
-###########################################################################
-#include_recipe "idautoapps::set_hostname"
-# This is currently causing issues.  If you change the hostname then you break the node link.
-
-###########################################################################
-# Modify sudoers                                                          #
-###########################################################################
-execute "sudoers-update" do
-	command "sudo sed -i 's|Defaults    requiretty|#Defaults    requiretty|g' /etc/sudoers"
-	creates "/tmp/sudoers-update.done"
-	not_if { ::File.exists?("/tmp/sudoers-update.done")}
-	action :run
-end
-
-###########################################################################
-# Manage users                                                            #
-###########################################################################
-user "config" do
-	action :create
-	comment "IdAuto Appliance configuration account"
-	shell "/bin/false"
-	password "$6$9wQiDrR3$hreGbkwNaPBqLPP2n/HbEeKMNlxt/Bq9.WQcTe7mvZh9LX0orrJ8TQixYqt.M9eEaqH5N.xSDJliLaC1ownii0"
-end
-
-user "tomcat" do
-	action :create
-	comment "Tomcat system user"
-	password "$6$NAsj56eO$rqhXtqlzGaHmj9qAJQgUH8HImdXc0TDGiOptdBr9T1OV1NyC3oDPBMiVorAxVxAhRKNFV6gbIgl7eLEqdyFYm0"
 end
 
 ###########################################################################
@@ -182,19 +141,65 @@ end
 
 
 ###########################################################################
+# Set timezone                                                            #
+###########################################################################
+timezone = "America/Chicago"
+
+link "/etc/localtime" do
+	to "/usr/share/zoneinfo/#{timezone}"
+	not_if "readlink /etc/localtime | grep -q '#{timezone}$'"
+end
+
+###########################################################################
+# Set hostname                                                            #
+###########################################################################
+#include_recipe "idautoapps::set_hostname"
+# This is currently causing issues.  If you change the hostname then you break the node link.
+
+###########################################################################
+# Manage users                                                            #
+###########################################################################
+# Create config user
+user "config" do
+	action :create
+	comment "IdAuto Appliance configuration account"
+	shell "/bin/false"
+	password "$6$9wQiDrR3$hreGbkwNaPBqLPP2n/HbEeKMNlxt/Bq9.WQcTe7mvZh9LX0orrJ8TQixYqt.M9eEaqH5N.xSDJliLaC1ownii0"
+end
+
+# Prepare config menu
+file "/var/opt/idauto/menu/menu.sh" do
+	mode "0755"
+	action :create_if_missing
+end
+
+# Modify config user shell
+user "config" do
+	shell "/var/opt/idauto/menu/menu.sh"
+	action :create
+end
+
+# Create tomcat user
+user "tomcat" do
+	action :create
+	comment "Tomcat system user"
+	password "$6$NAsj56eO$rqhXtqlzGaHmj9qAJQgUH8HImdXc0TDGiOptdBr9T1OV1NyC3oDPBMiVorAxVxAhRKNFV6gbIgl7eLEqdyFYm0"
+end
+
+###########################################################################
 # Modify SSH                                                              #
 ###########################################################################
 execute "sudoers-permitrootlogin" do
 	command "sudo sed -i 's|^PermitRootLogin forced-commands-only|\\#PermitRootLogin forced-commands-only|g' /etc/ssh/sshd_config"
-	creates "/tmp/sudoers-permitrootlogin.done"
-	not_if { ::File.exists?("/tmp/sudoers-permitrootlogin.done")}
+	creates "/root/results/sudoers-permitrootlogin.done"
+	not_if { ::File.exists?("/root/results/sudoers-permitrootlogin.done")}
 	action :run
 end
 
 execute "sudoers-maxauthtries" do
 	command "sudo sed -i 's|^#MaxAuthTries 6|\\MaxAuthTries 30|g' /etc/ssh/sshd_config"
-	creates "/tmp/sudoers-maxauthtries.done"
-	not_if { ::File.exists?("/tmp/sudoers-maxauthtries.done")}
+	creates "/root/results/sudoers-maxauthtries.done"
+	not_if { ::File.exists?("/root/results/sudoers-maxauthtries.done")}
 	action :run
 end
 
@@ -204,8 +209,8 @@ end
 
 execute "copy_authorized_keys" do
 	command "sudo cp ~ec2-user/.ssh/authorized_keys /root/.ssh/"
-	creates "/tmp/copy_authorized_keys.done"
-	not_if { ::File.exists?("/tmp/copy_authorized_keys.done")}
+	creates "/root/results/copy_authorized_keys.done"
+	not_if { ::File.exists?("/root/results/copy_authorized_keys.done")}
 	action :run
 end
 
@@ -219,23 +224,23 @@ end
 # Install WinEXE
 execute "install_winexe" do
 	command "sudo rpm -qa | grep -qw winexe || rpm -ivh https://s3.amazonaws.com/idauto-apps/winexe-1.00-2.2.x86_64.rpm"
-	creates "/tmp/install_winexe.done"
-	not_if { ::File.exists?("/tmp/install_winexe.done")}
+	creates "/root/results/install_winexe.done"
+	not_if { ::File.exists?("/root/results/install_winexe.done")}
 	action :run
 end
 
 # Install Java
 execute "install_java" do
 	command "sudo rpm -qa | grep -qw jre || rpm -ivh https://s3.amazonaws.com/idauto-apps/jre-7u71-linux-x64.rpm"
-	creates "/tmp/install_java.done"
-	not_if { ::File.exists?("/tmp/install_java.done")}
+	creates "/root/results/install_java.done"
+	not_if { ::File.exists?("/root/results/install_java.done")}
 	action :run
 end
 
 execute "enable_java" do
 	command "sudo /usr/sbin/alternatives --install /usr/bin/java java /usr/java/latest/bin/java 20000"
-	creates "/tmp/enable_java.done"
-	not_if { ::File.exists?("/tmp/enable_java.done")}
+	creates "/root/results/enable_java.done"
+	not_if { ::File.exists?("/root/results/enable_java.done")}
 	action :run
 end
 
@@ -256,39 +261,182 @@ bash "extract_connector" do
 	not_if { ::File.exists?("/var/opt/idauto/common/lib/mysql-connector-java-#{mysql_connector_major}.#{mysql_connector_minor}-bin.jar")}
 end
 
-# Install Tomcat
-remote_file "/root/install-and-configure-tomcat.sh" do
-  source "https://s3.amazonaws.com/idauto-apps/install-and-configure-tomcat.sh"
+###########################################################################
+# Install and Configure Tomcat                                            #
+###########################################################################
+tomcat_version = "7.0.56"
+
+# Download Tomcat
+remote_file "/root/apache-tomcat-#{tomcat_version}.zip" do
+  source "https://s3.amazonaws.com/idauto-apps/apache-tomcat-#{tomcat_version}.zip"
   mode '0755'
-  not_if { ::File.exists?("/root/install-and-configure-tomcat.sh") }
+  not_if { ::File.exists?("/root/apache-tomcat-#{tomcat_version}.zip") }
 end
 
-### Need to break out sh script into separate resources.
-execute "install-and-configure-tomcat" do
-	command "/root/install-and-configure-tomcat.sh"
-	creates "/tmp/install-and-configure-tomcat.done"
-	not_if { ::File.exists?("/tmp/install-and-configure-tomcat.done")}
-	action :run
+# Deploy Tomcat
+bash "deploy_tomcat" do
+	code <<-EOH
+		unzip -o /root/apache-tomcat-#{tomcat_version}.zip -d /srv
+		mv /srv/`basename /root/apache-tomcat-#{tomcat_version}.zip .zip` /srv/tomcat7
+		touch /srv/tomcat7/logs/catalina.out
+		ln -s /srv/tomcat7/logs/catalina.out /var/opt/idauto/logs/tomcat.log
+		rm -rf /srv/tomcat7/bin/*.bat
+		EOH
+	creates "/root/results/deploy_tomcat_#{tomcat_version}.done"
+	not_if { ::File.exists?("/root/results/deploy_tomcat_#{tomcat_version}.done")}
 end
 
+# Download Tomcat configuration files
+remote_file "/etc/init.d/tomcat" do
+  source "https://s3.amazonaws.com/idauto-apps/tomcat_init.d"
+  mode '0755'
+end
+
+remote_file "/etc/logrotate.d/tomcat" do
+  source "https://s3.amazonaws.com/idauto-apps/tomcat_logrotate"
+  mode '0644'
+end
+
+remote_file "/var/opt/idauto/certs/tomcat_keystore.jks" do
+  source "https://s3.amazonaws.com/idauto-apps/tomcat_keystore.jks"
+  mode '0644'
+end
+
+remote_file "/srv/tomcat7/conf/server.xml" do
+  source "https://s3.amazonaws.com/idauto-apps/server.xml"
+  mode '0644'
+end
+
+remote_file "/srv/tomcat7/conf/catalina.properties" do
+  source "https://s3.amazonaws.com/idauto-apps/catalina.properties"
+  mode '0644'
+end
+
+remote_file "/srv/tomcat7/lib/catalina-jmx-remote.jar" do
+  source "https://s3.amazonaws.com/idauto-apps/catalina-jmx-remote.jar"
+  mode '0644'
+end
+
+remote_file "/var/opt/idauto/common/jmxremote.access" do
+  source "https://s3.amazonaws.com/idauto-apps/jmxremote.access"
+  mode '0644'
+end
+
+remote_file "/var/opt/idauto/common/jmxremote.password" do
+  source "https://s3.amazonaws.com/idauto-apps/jmxremote.password"
+  mode '0644'
+end
+
+# Update Tomcat directories
+directory "/srv/tomcat7/bin" do
+	mode "0755"
+	action :create
+end
+
+# Tomcat service
+service "tomcat" do
+	supports :status => true, :restart => true, :reload => true
+	action [ :enable, :start ]
+end
+
+# Modify tomcat sleep settings
 execute "modify-tomcat-sleep" do
 	command "sudo sed -i -e 's/sleep 60/sleep 3/' /etc/init.d/tomcat"
-	creates "/tmp/modify-tomcat-sleep.done"
-	not_if { ::File.exists?("/tmp/modify-tomcat-sleep.done")}
+	creates "/root/results/modify-tomcat-sleep.done"
+	not_if { ::File.exists?("/root/results/modify-tomcat-sleep.done")}
 	action :run
 end
 
-remote_file "/root/install-and-configure-apps-and-cron.sh" do
-  source "https://s3.amazonaws.com/idauto-apps/install-and-configure-apps-and-cron.sh"
+###########################################################################
+# Configure Base Apps                                                     #
+###########################################################################
+# Install ROOT
+root_version = "1.0.2"
+
+remote_file "/root/ROOT-#{root_version}.zip" do
+  source "http://downloads.identitymgmt.net/bundles/ROOT-#{root_version}.zip"
   mode '0755'
-  not_if { ::File.exists?("/root/install-and-configure-apps-and-cron.sh") }
+  not_if { ::File.exists?("/root/ROOT-#{root_version}.zip") }
 end
 
-### This portion has killed sudoers file!  Need to break out sh script into separate resources.
-execute "install-and-configure-apps-and-cron.sh" do
-	command "/root/install-and-configure-apps-and-cron.sh"
-	creates "/tmp/install-and-configure-apps-and-cron.done"
-	not_if { ::File.exists?("/tmp/install-and-configure-apps-and-cron.done")}
+execute "deploy-root-app" do
+	command "sudo unzip -o /root/ROOT-#{root_version}.zip -d /var/opt/idauto/webapps"
+	creates "/root/results/deploy-root-#{root_version}.done"
+	not_if { ::File.exists?("/root/results/deploy-root-#{root_version}.done")}
 	action :run
 end
 
+# Install Appliance Manager
+idautoapps_version = "3.1.5.3.10"
+
+remote_file "/root/idauto-apps-#{idautoapps_version}.zip" do
+  source "http://downloads.identitymgmt.net/bundles-30/idauto-apps-#{idautoapps_version}.zip"
+  mode '0755'
+  not_if { ::File.exists?("/root/idauto-apps-#{idautoapps_version}.zip") }
+end
+
+execute "deploy-idautoapps-app" do
+	command "sudo unzip -o /root/idauto-apps-#{idautoapps_version}.zip -d /var/opt/idauto/webapps"
+	creates "/root/results/deploy-idautoapps-#{idautoapps_version}.done"
+	not_if { ::File.exists?("/root/results/deploy-idautoapps-#{idautoapps_version}.done")}
+	action :run
+end
+
+###########################################################################
+# Update File Permissions                                                 #
+###########################################################################
+# Modify ownership of idauto directory
+directory "/var/opt/idauto" do
+	owner "tomcat"
+	group "tomcat"
+	recursive true
+	action :create
+end
+
+# Modify ownership of tomcat directory
+directory "/srv/tomcat7" do
+	owner "tomcat"
+	group "tomcat"
+	recursive true
+	action :create
+end
+
+###########################################################################
+# Configure sudoers                                                       #
+###########################################################################
+# Install config user sudoers files
+remote_file "/var/opt/idauto/menu/sudoers.config" do
+  source "https://s3.amazonaws.com/idauto-apps/sudoers.config"
+  owner "root"
+  group "root"
+  mode '0440'
+end
+
+# Install tomcat user sudoers files
+remote_file "/var/opt/idauto/tomcat/sudoers.tomcat" do
+  source "https://s3.amazonaws.com/idauto-apps/sudoers.tomcat"
+  owner "root"
+  group "root"
+  mode '0440'
+end
+
+# Replace sudoers file
+remote_file "/etc/sudoers" do
+  source "https://s3.amazonaws.com/idauto-apps/sudoers"
+  mode '0440'
+end
+
+###########################################################################
+# Reboot Now                                                              #
+###########################################################################
+###@@@  Chef didn't like the :nothing action so have to figure out a way to only reboot on first run
+#reboot "on_first_install" do
+#  action :nothing
+#  reason "Reboot after basic IdAuto Appliance setup."
+#end
+
+#execute "reboot_after_install" do
+#  creates "/root/results/reboot_after_install.done"
+#  notifies :reboot_now, 'reboot[on_first_install]', :immediately
+#  not_if { ::File.exists?("/root/results/reboot_after_install.done")}
+#end
