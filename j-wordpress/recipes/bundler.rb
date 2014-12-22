50642ec7a702a7af939008dbb02d48d59591b36c
jseiser <justinseiser@gmail.com>
2014-12-22 09:17:09 -0500

    Added Node, Bundler

---
+++ j-wordpress/recipes/bundler.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: j-wordpress
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+gem_package "bundler" do
+  action :install
+end
