266cbc5f383a3435b6f877e73a6a565df52552ee
jseiser <justinseiser@gmail.com>
2014-12-22 09:50:36 -0500

    Bundle Install, git

---
+++ j-wordpress/recipes/gitwww.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: j-wordpress
+# Recipe:: default
+#
+# Copyright 2014, gitwww
+#
+# All rights reserved - Do Not Redistribute
+#
+git "/var/www/html" do
+  repository "git://github.com:IdAuto/wordpresswww.git"
+  revision "justin"
+  action :sync
+end
+
+execute "bundle install" do
+	cwd "/var/www/html"
+	command "bundle install"
+	action :nothing
+end
+
+
+execute "composer install" do
+	cwd "/var/www/html"
+	command "composer install"
+	action :nothing
+end
+
+execute "npm install" do
+	cwd "/var/www/html/web/app/themes/themia"
+	command "npm install"
+	notifies :restart, "service[httpd]", :immediately
+end
